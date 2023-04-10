374416bb0f2791681c7ae5c3029796d84828ac05
Viet Anh <phamhoangviet.anh136@gmail.com>
2023-04-10 14:45:36 +0700

    .

--- GiveAndReceive/Areas/Admin/AdminAreaRegistration.cs
+++ GiveAndReceive/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace GiveAndReceive.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", controller = "home", id = UrlParameter.Optional },
+                namespaces: new[] { "GiveAndReceive.Areas.Admin.Controllers" }
             );
         }
     }
