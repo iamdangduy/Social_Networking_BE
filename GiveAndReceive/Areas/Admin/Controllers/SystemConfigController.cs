1c699425f96a073cc16b72840a902a3d55c8cf34
Nguyenphu1807 <Nguyenphuyh2812@gmail.com>
2023-04-11 17:35:10 +0700

    .

---
+++ GiveAndReceive/Areas/Admin/Controllers/SystemConfigController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiveAndReceive.Areas.Admin.Controllers
+{
+    public class SystemConfigController : Controller
+    {
+        // GET: Admin/SystemConfig
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
