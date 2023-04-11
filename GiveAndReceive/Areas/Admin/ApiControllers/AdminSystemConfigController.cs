bc80ce3f0c24400736dbf4540e3e1b90e29bd673
Nguyenphu1807 <Nguyenphuyh2812@gmail.com>
2023-04-11 16:19:52 +0700

    .

--- GiveAndReceive/Areas/Admin/ApiControllers/AdminSystemConfigController.cs
+++ GiveAndReceive/Areas/Admin/ApiControllers/AdminSystemConfigController.cs
@@ -67,10 +67,7 @@ namespace GiveAndReceive.Areas.Admin.ApiControllers
                         transaction.Commit();
                         return Success(null, "Sửa thành công!");
                     }
-
                 }
-
-
             }
             catch (Exception ex)
             {
