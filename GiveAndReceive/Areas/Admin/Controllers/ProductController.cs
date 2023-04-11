bec9a9429f5545aae90c42f1cd22c70bb09f64e2
Dai <dophucdai3005@gmail.com>
2023-04-11 11:07:38 +0700

    .

---
+++ GiveAndReceive/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiveAndReceive.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        public ActionResult UpdateProduct(string id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
