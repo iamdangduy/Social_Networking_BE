76ea865d5bad9f5a195311d7104ee2ea2df8ac6a
Dai <dophucdai3005@gmail.com>
2023-04-12 10:05:15 +0700

    .

--- GiveAndReceive/Services/NotificationService.cs
+++ GiveAndReceive/Services/NotificationService.cs
@@ -21,5 +21,18 @@ namespace GiveAndReceive.Services
             List<Notification> ls = this._connection.Query<Notification>(query, new { userId = userId }).ToList();
             return ls;
         }
+
+        public Notification GetNotificationById(string id, IDbTransaction transaction = null)
+        {
+            string query = "select * from notification where NotificationId = @id";
+            return this._connection.Query<Notification>(query, new { id }, transaction).FirstOrDefault();
+        }
+
+        public void UpdateNotificationRead(Notification model, IDbTransaction transaction = null)
+        {
+            string query = "UPDATE [dbo].[notification] SET IsRead = @IsRead WHERE NotificationId = @NotificationId";
+            int status = this._connection.Execute(query, model, transaction);
+            if (status <= 0) throw new Exception(JsonResult.Message.ERROR_SYSTEM);
+        }
     }
 }
