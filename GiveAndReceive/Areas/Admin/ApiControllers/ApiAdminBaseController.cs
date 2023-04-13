c66f114edd5cf4079a6e47b2da0320269ba3309e
Nguyenphu1807 <Nguyenphuyh2812@gmail.com>
2023-04-13 13:53:55 +0700

    .

--- GiveAndReceive/Areas/Admin/ApiControllers/ApiAdminBaseController.cs
+++ GiveAndReceive/Areas/Admin/ApiControllers/ApiAdminBaseController.cs
@@ -10,7 +10,7 @@ using System.Web.Http;
 
 namespace GiveAndReceive.Areas.Admin.ApiControllers
 {
-    [APIAuthenticationFilter]
+    [ApiAdminTokenRequire]
     public class ApiAdminBaseController : ApiController
     {
         [HttpGet]
