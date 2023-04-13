2846cc9acee955501b25b83e8613cf78194cf662
Dai <dophucdai3005@gmail.com>
2023-04-13 09:48:08 +0700

    .

---
+++ GiveAndReceive/Controllers/UserBankInfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiveAndReceive.Controllers
+{
+    public class UserBankInfoController : Controller
+    {
+        // GET: UserBankInfo
+        [Route("nguoi-dung/tai-khoan-ngan-hang")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
