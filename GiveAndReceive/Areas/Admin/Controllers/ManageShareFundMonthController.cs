06d2313baef4c1c7cf05a36e14f72531711ea0d5
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-04-13 14:55:53 +0700

    . (#70)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

---
+++ GiveAndReceive/Areas/Admin/Controllers/ManageShareFundMonthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiveAndReceive.Areas.Admin.Controllers
+{
+    public class ManageShareFundMonthController : Controller
+    {
+        // GET: Admin/ManageShareFundMonth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
