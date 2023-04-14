7ef2855b181f7a3d37146b6652fe607f03941b43
Duy Nguyen <nguyendangduy18082001@gmail.com>
2023-04-14 10:22:04 +0700

    .

--- GiveAndReceive/Areas/Admin/ApiControllers/AdminUserPropertiesController.cs
+++ GiveAndReceive/Areas/Admin/ApiControllers/AdminUserPropertiesController.cs
@@ -1,6 +1,8 @@
 ﻿using GiveAndReceive.ApiControllers;
+using GiveAndReceive.Areas.Admin.Services;
 using GiveAndReceive.Filters;
 using GiveAndReceive.Models;
+using GiveAndReceive.Providers;
 using GiveAndReceive.Services;
 using System;
 using System.Collections.Generic;
@@ -29,6 +31,95 @@ namespace GiveAndReceive.Areas.Admin.ApiControllers
                     }
                 }
 
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ApiAdminTokenRequire]
+        public JsonResult UpdateIdentityToSystemDecline(string UserId, string Reason)
+        {
+            try
+            {
+                using (var connect = BaseService.Connect())
+                {
+                    connect.Open();
+                    using (var transaction = connect.BeginTransaction())
+                    {
+
+                        UserAdmin userAdmin = SecurityProvider.GetUserAdminByToken(Request);
+                        if (userAdmin == null) return Unauthorized();
+
+                        AdminUserPropertiesService adminUserPropertiesService = new AdminUserPropertiesService(connect);
+                        var UserProperties = adminUserPropertiesService.GetUserPropertiesByUserId(UserId, transaction);
+                        if (UserProperties == null) return Error();
+                        if (UserProperties.IdentificationApprove != UserProperties.EnumIdentificationApprove.SYSTEM_ACCEPT) return Error();
+
+                        //tạo thông báo
+                        //Notification notification = new Notification
+                        //{
+                        //    UserId = UserId,
+                        //    Title = "Thông báo",
+                        //    Message = "CMND của bạn đã bị từ chối vì: " + Reason,
+                        //    IsRead = false,
+                        //    CreateTime = HelperProvider.GetSeconds(),
+                        //};
+                        //AdminNotificationService adminNotificationService = new AdminNotificationService(connect);
+                        //if (!adminNotificationService.InsertNotification(notification, transaction)) return Error();
+
+                        //if (!adminUserPropertiesService.UpdateStatusUserIdentity(UserId, UserProperties.EnumApprove.SYSTEM_DECLINE, transaction)) return Error();
+
+                        transaction.Commit();
+                        return Success();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ApiAdminTokenRequire]
+        public JsonResult UpdateIdentityToSuccess(string UserId)
+        {
+            try
+            {
+                using (var connect = BaseService.Connect())
+                {
+                    connect.Open();
+                    using (var transaction = connect.BeginTransaction())
+                    {
+
+                        UserAdmin userAdmin = SecurityProvider.GetUserAdminByToken(Request);
+                        if (userAdmin == null) return Unauthorized();
+
+                        AdminUserPropertiesService adminUserPropertiesService = new AdminUserPropertiesService(connect);
+                        var UserProperties = adminUserPropertiesService.GetUserPropertiesByUserId(UserId, transaction);
+                        if (UserProperties == null) return Error();
+                        if (UserProperties.IdentificationApprove != UserProperties.EnumIdentificationApprove.SYSTEM_DECLINE) return Error();
+                        //if (!) return Error();
+                        adminUserPropertiesService.UpdateStatusUserIdentity(UserId, UserProperties.EnumIdentificationApprove.SYSTEM_ACCEPT, transaction)
+                        //tạo thông báo
+                        //Notification notification = new Notification
+                        //{
+                        //    UserId = UserId,
+                        //    Title = "Thông báo",
+                        //    Message = "Thông tin của bạn đã được xác thực.",
+                        //    IsRead = false,
+                        //    CreateTime = HelperProvider.GetSeconds(),
+                        //};
+                        //AdminNotificationService adminNotificationService = new AdminNotificationService(connect);
+                        //if (!adminNotificationService.InsertNotification(notification, transaction)) return Error();
+
+                        transaction.Commit();
+                        return Success();
+                    }
+                }
             }
             catch (Exception ex)
             {
